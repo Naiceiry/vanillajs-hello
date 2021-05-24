72d8ad85493387bfbf70a6398d7b756f3fb0f1e1
Naiceiry <naiceirymp@gmail.com>
2021-05-24 15:07:53 +0000

    actualizacion Nay

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
+/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn_NE").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = newExcusa();
+  });
+};
+let getRandonElement = arr => arr[Math.floor(Math.randon() * arr.length)];
+let newExcusa = () => {
+  let who = ["El Perro", "Mi Abuelita", "su totuga", "mi pajaro"];
+  let what = ["come", "molesta", "aplasta", "rompio"];
+  let when = [
+    "antes de la clase",
+    "derecha termine",
+    "durante mi almuerzo",
+    "mientras rezaba"
+  ];
+  return (
+    getRandonElement(who) +
+    " " +
+    getRandonElement(what) +
+    " " +
+    getRandonElement(when)
+  );
 };
