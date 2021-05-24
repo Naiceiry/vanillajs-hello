6990b2a76c16871d66501ccd52595b456e7854ce
Naiceiry <naiceirymp@gmail.com>
2021-05-24 15:11:54 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -3,26 +3,23 @@ import "bootstrap";
 import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+
 window.onload = function() {
-  document.querySelector("#btn_NE").addEventListener("click", () => {
-    document.querySelector("#excusa").innerHTML = newExcusa();
+  document.querySelector("#nuevaexcusa").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generador();
   });
 };
-let getRandonElement = arr => arr[Math.floor(Math.randon() * arr.length)];
-let newExcusa = () => {
-  let who = ["El Perro", "Mi Abuelita", "su totuga", "mi pajaro"];
-  let what = ["come", "molesta", "aplasta", "rompio"];
-  let when = [
+let aleatiriedad = arr => arr[Math.floor(Math.random() * arr.length)];
+let generador = () => {
+  let quien = ["El Perro", "Mi Abuelita", "su totuga", "mi pajaro"];
+  let que = ["come", "molesta", "aplasta", "rompio"];
+  let donde = [
     "antes de la clase",
     "derecha termine",
     "durante mi almuerzo",
     "mientras rezaba"
   ];
   return (
-    getRandonElement(who) +
-    " " +
-    getRandonElement(what) +
-    " " +
-    getRandonElement(when)
+    aleatiriedad(quien) + " " + aleatiriedad(que) + " " + aleatiriedad(donde)
   );
 };
